f73e078a03e10fbe23979c4489bd51083dde2325
ashestriumph <jammelsandani@protonmail.ch>
2025-04-01 01:20:26 +0700

    (feat): showing article if it exist in the constant declaration which are located in article-content file

--- client/src/pages/Article.js
+++ client/src/pages/Article.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import articleContent from './article-content';
 
 const Article = () => {
+  const { name } = useParams();
+  const article = articleContent.find((article) => article.name === name);
+  if (!article) {
+    return <h1 className="m-6 text-2xl font-bold text-gray-900 sm:text-4xl">Article not found</h1>;
+  }
   return (
-    <h1 className="m-6 text-2xl font-bold text-gray-900 sm:text-4xl">This is article</h1>
+    <div>
+    <h1 className="m-6 text-2xl font-bold text-gray-900 sm:text-4xl">
+      {article.title}
+      </h1>
+      {article.content.map((paragraph, index) => (
+        <p className="mx-auto mb-4 text-base leading-relaxed" key={index}>
+          {paragraph}
+        </p>
+      ))}
+    </div>
   )
 }
 
