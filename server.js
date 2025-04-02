4c3e5d2df19dcea383c32aa03b320602fbabb859
ashestriumph <jammelsandani@protonmail.ch>
2025-04-02 15:07:33 +0700

    (feat): test express server with postman

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const express = require('express'); // import express
 const app = express(); // create an express app
 const PORT = process.env.PORT || 8000; // set the port
 
+// Initialize middleware
+// Middleware to parse JSON bodies
+app.use(express.json({extended: false})); // parse JSON bodies
+// app.use(express.urlencoded({ extended: false })); // parse URL-encoded bodies
+
 // Test route
 app.get('/', (req, res) => {
   res.send('Hello World!'); // send a response
+});
+app.post('/', (req, res) => {
+  res.send(`Hello ${req.body.name}`); // send a response
 });
 
 // Start the server
